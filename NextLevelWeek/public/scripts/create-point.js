c9606919d2e92a31582431d351dac1a607dd5e34
Maurício Portilho Soares <mportilhosoares@yahoo.com>
2020-06-16 17:13:49 -0300

    Add files via upload

---
+++ NextLevelWeek/public/scripts/create-point.js
@@ -0,0 +1,104 @@
+
+
+function populateUFs () {
+    const ufSelect = document.querySelector("select[name=uf]")
+
+    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome')
+    .then( (res) => {return res.json() })
+    .then( states => {
+
+        for( const state of states ) {
+            ufSelect.innerHTML += `<option value = "${state.id}">${state.nome}</option>`
+        }
+
+
+    } )
+}
+
+populateUFs()
+
+
+function getCities (event) {
+    const citySelect = document.querySelector("[name=city]")
+    const stateInput = document.querySelector("[name=state]")
+
+    const ufValue = event.target.value
+
+    const indexOfSelectedState = event.target.selectedIndex
+    stateInput.value = event.target.options[indexOfSelectedState].text
+
+
+    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+
+    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+    citySelect.disabled = true
+
+    fetch(url)
+    .then( (res) => {return res.json() })
+    .then( cities => {
+        
+        for( const city of cities ) {
+            citySelect.innerHTML += `<option value = "${city.nome}">${city.nome}</option>`
+        }
+
+        citySelect.disabled = false
+    } )
+}
+
+
+
+document
+    .querySelector("select[name=uf]")
+    .addEventListener("change", getCities)
+
+
+    // itens de Coleta
+//pegar todos os li
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+    item.addEventListener("click", handleSelectedItem)
+}
+
+const collectedItems = document.querySelector("input[name=items]")
+
+// variavel pode ser "let" ou "var"
+let selectedItems = []; //Array pois está com []...coleção de dados
+
+function handleSelectedItem(event) {
+    const itemLi = event.target
+    
+    //add or remove a class using "toggle"
+    itemLi.classList.toggle("selected") 
+
+    const itemId = itemLi.dataset.id
+
+    
+
+
+    // verificar se existem itens selecionados, se sim
+    // pagar os items selecionados
+
+    const alreadySelected = selectedItems.findIndex( item => {
+        const itemFound = item === itemId //será true or false
+        return itemFound
+    })
+
+    
+    //se já estiver selecionado, tirar da selecao
+    if(alreadySelected >= 0 ) { //Zero pq a posicao de array começa no Zero
+        const filteredItems = selectedItems.filter( item =>{
+            const itemIsDifferent = item != itemId //isso retorna o falso se for igual então ele tira
+            return itemIsDifferent
+        })
+
+        selectedItems = filteredItems        
+    } else { //se não estiver selecionado, adiconar a selecao
+        selectedItems.push(itemId)
+    }
+    
+    // atualizar o campo escondido com os items selecionados
+    collectedItems.value = selectedItems
+    
+}
---
+++ NextLevelWeek/public/scripts/intro.js
@@ -0,0 +1,38 @@
+// comments
+// document.write("Hello")
+
+// variaveis, tipos de dados
+// variaveis podem ser "var" ou "let"
+// let myvar = "He"
+// const myconst = "He"
+
+// document.write(myconst + myvar)
+
+// string usa "" ou '' ou entre crases ``
+// const s1 = "ISso é uma string"
+// const s2 = 'blabala'
+
+// boolean - true ou false
+// const bTrue = true
+// consta bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//  altura:"1,80m",
+//  idade: 24,
+//  solteiro: true,
+//  correr(){
+//      return "run Forest"
+//    } <--fechando do correr
+// } <--fechando da const pessoa
+// document.write(pessoa.correr()) <- executa o objeto e a funcionalidade pedida
+
+// Array - Vetores
+// const blaBlaBla = ["blue", 2, {name: "My Name"}]
+//      document.write(blaBlaBla[posição desejada].name) <---O ".name é pra chamar a propriedade 'My name' do objeto name "
+
+// loop repetições
+// for (i = 0; i < 10; i++) {
+//      document.write(`<p>${i}</p>`)
+// }
